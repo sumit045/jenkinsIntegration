e451236766a465d099c447bcb5d3ef9f70d44786
sumit045 <sumit619.hellboy@gmail.com>
2020-03-05 21:16:17 +0530

    ho gya

--- Jenkins_GIT/src/main/java/sumitPack/jenkins.java
+++ Jenkins_GIT/src/main/java/sumitPack/jenkins.java
@@ -1,5 +1,7 @@
 package sumitPack;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class jenkins {
@@ -8,6 +10,11 @@ public class jenkins {
 	public void test()
 	{
 		System.out.println("Test success hoga");
+		String path = "C:\\Users\\SumitKumar\\git\\repositoryJenkins\\Jenkins_GIT\\Drivers\\geckodriver.exe";
+		System.setProperty("webdriver.gecko.driver", path);
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://google.com");
 	}
 
 }
