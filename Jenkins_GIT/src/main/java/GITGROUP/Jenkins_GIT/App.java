45cde95505edb5b5f9f162984955ff52274f877e
sumit045 <sumit619.hellboy@gmail.com>
2020-03-05 19:46:42 +0530

    First commit

---
+++ Jenkins_GIT/src/main/java/GITGROUP/Jenkins_GIT/App.java
@@ -0,0 +1,13 @@
+package GITGROUP.Jenkins_GIT;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Jenkins_GIT/src/main/java/sumitPack/jenkins.java
@@ -0,0 +1,10 @@
+package sumitPack;
+
+public class jenkins {
+
+	public void test()
+	{
+		System.out.println("Test is successful");
+	}
+	
+}
