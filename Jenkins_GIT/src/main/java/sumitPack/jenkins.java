191bee011374b4d2aea0c4f021f10fab08e1217c
sumit045 <sumit619.hellboy@gmail.com>
2020-03-15 23:38:44 +0530

    Changes in path

--- Jenkins_GIT/src/main/java/sumitPack/jenkins.java
+++ Jenkins_GIT/src/main/java/sumitPack/jenkins.java
@@ -6,11 +6,15 @@ import org.testng.annotations.Test;
 
 public class jenkins {
 	
+	/*public static void main(String[] args) {
+		System.out.println(System.getProperty("user.dir"));
+	}*/
+	
 	@Test
 	public void test()
 	{
 		System.out.println("Test success hoga");
-		String path = "C:\\Users\\SumitKumar\\git\\repositoryJenkins\\Jenkins_GIT\\Drivers\\geckodriver.exe";
+		String path = System.getProperty("user.dir")+ "\\Drivers\\geckodriver.exe";
 		System.setProperty("webdriver.gecko.driver", path);
 		
 		WebDriver driver = new FirefoxDriver();
