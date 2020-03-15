4cca27a27e29698cb542fcb4c004bb8ef56b820a
sumit045 <sumit619.hellboy@gmail.com>
2020-03-16 00:02:53 +0530

    added chrome

--- Jenkins_GIT/src/main/java/sumitPack/jenkins.java
+++ Jenkins_GIT/src/main/java/sumitPack/jenkins.java
@@ -3,6 +3,7 @@ package sumitPack;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
@@ -16,10 +17,11 @@ public class jenkins {
 	public void test()
 	{
 		System.out.println("Test success hoga");
-		String path = System.getProperty("user.dir")+ "\\Drivers\\geckodriver.exe";
-		System.setProperty("webdriver.gecko.driver", path);
+		String path = System.getProperty("user.dir")+ "\\Drivers\\chromedriver.exe";
+		//String path = System.getProperty("user.dir")+ "\\Drivers\\geckodriver.exe";
+		System.setProperty("webdriver.chrome.driver", path);
 		
-		WebDriver driver = new FirefoxDriver();
+		WebDriver driver = new ChromeDriver();
 		driver.get("http://google.com");
 		driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
 		System.out.println(driver.getTitle());
